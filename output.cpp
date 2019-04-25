057c48035140d57f0097b684d9c735289ff51590
timmyhsu2 <47243108+timmyhsu2@users.noreply.github.com>
2019-04-26 03:47:39 +0800

    Update output.cpp

--- output.cpp
+++ output.cpp
@@ -3,15 +3,20 @@
 #include <iomanip>
 #include <fstream>
 #include <vector>
-#include "item.h"
-#include "output.h"
+#include <iomanip>
+#include "functions.h"
 using namespace std;
 
 void write_list (string output_name, vector<item> &item)
 {
   ofstream output(output_name.c_str());
   for (int i = 0; i < item.size(); i++){
-    output<<item[i].name<<"     "<<item[i].flag<<"     "<<item[i].stock<<"     "<<item[i].price<<"     "<<item[i].shops<<'\n';
+    output<<left;
+    output<<setw(10)<<item[i].name;
+    output<<setw(20)<<item[i].flag;
+    output<<setw(5)<<item[i].stock;
+    output<<setw(5)<<item[i].price;
+    output<<setw(10)<<item[i].shops<<'\n';
   }
   output.close();
 }
