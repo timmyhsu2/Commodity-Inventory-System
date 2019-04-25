d8f3698fbd362d31b502c249eb358b93aa8d425e
eric141822 <47049224+eric141822@users.noreply.github.com>
2019-04-25 23:20:13 +0800

    Added structure Item.
    Included name, flag, stock, price, shops, sold-out times.

---
+++ item.h
@@ -0,0 +1,14 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <string>
+using namespace std;
+struct item{
+  string name;
+  string flag;
+  int stock;
+  double price;
+  string shops;
+  int soldout;
+};
+#endif
